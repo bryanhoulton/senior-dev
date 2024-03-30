74b72d19730906e61c477ba11a604c86dc1f4dc4
Bryan Houlton <brhoulton@gmail.com>
2024-03-30 13:52:20 -0400

    fix(auth): parsing

--- app/auth.ts
+++ app/auth.ts
@@ -2,8 +2,9 @@ import { createAppAuth } from '@octokit/auth-app';
 
 const auth = createAppAuth({
   appId: process.env.GITHUB_APP_ID || 0,
-  privateKey:
-    JSON.parse(process.env.GITHUB_APP_PRIVATE_KEY || "{}").value || "",
+  privateKey: JSON.parse(
+    process.env.GITHUB_APP_PRIVATE_KEY || ""
+  ).value.replace(/\\n/g, "\n"),
   clientId: process.env.GITHUB_APP_CLIENT_ID,
   clientSecret: process.env.GITHUB_APP_CLIENT_SECRET,
 });
